3154946d7b21e81b4582c65269d35f32710cab45
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-24 00:57:23 +0530

    Added datetime and token in the schema

--- utils/blogschema.sql
+++ utils/blogschema.sql
@@ -4,13 +4,14 @@ drop table if exists users;
 create table posts (
     id integer primary key autoincrement, 
     title text not null,
-    content text not null
-    -- created at
+    content text not null,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 create table users (
     id integer primary key autoincrement,
     username text not null,
-    pw_hash text not null
-    -- created at 
+    pw_hash text not null,
+    token text default null,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
