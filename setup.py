a962d4e75d2ff408371c4644299782ef77dda97c
Prakhar Srivastav <prakhar1989@gmail.com>
2014-03-24 20:21:49 +0530

    Added a setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from sqlite3 import dbapi2 as sqlite3
+import os
+
+DATABASE = os.path.join('db/postman_demo.db')
+
+def connect_db():
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def setup_db():
+    db = connect_db()
+    with open('utils/blogschema.sql', "r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    print "---- DATABASE CLEARED ----"
+
+if __name__ == "__main__":
+    setup_db()
